c575802a0d33d34a685a91c6a9f3d62ce8a28d62
lal15aditi@gmail.com <souravgarai38@gmail.com>
2023-12-16 19:44:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyForm from './components/MyForm'
+
+function App() {
+    return (
+        <div className='app'>
+            <MyForm />
+        </div>
+    )
+}
+
+export default App
